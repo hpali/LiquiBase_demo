7d89df926b964e4ed50db4cc58b5221ca45391e7
hpali <hidy.pal@gmail.com>
2022-05-10 09:06:41 +0200

    Initial commit

---
+++ src/main/java/home/hidy/demo_liquibase/LiquiBaseDemoApplication.java
@@ -0,0 +1,13 @@
+package home.hidy.demo_liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquiBaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquiBaseDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/home/hidy/demo_liquibase/LiquiBaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package home.hidy.demo_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquiBaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
